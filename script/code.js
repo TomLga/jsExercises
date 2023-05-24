9794ca7f9a1c66db4348f5831795429a8bac7f23
TomLga <semitallbox@gmail.com>
2023-05-24 16:13:34 +0200

    q2

---
+++ script/code.js
@@ -0,0 +1,21 @@
+let person = {
+    fristName : "logan",
+    lastName : "Thomas",
+    age : 23,
+}
+
+
+let subjects = ['english ','afrikaans','Maths','life science'];
+
+let adress = {
+    streetName : "ABCrest",
+    streetNumb: 125,
+    suburb: "Pinelands,",
+    city: "Cape Town",
+    CountryName: "South Africa"
+}
+
+
+console.log(person)
+console.log(subjects);
+console.log(adress);
