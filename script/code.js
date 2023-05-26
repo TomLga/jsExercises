720ad48f15e00d98f73240583f25c3aa190ecd18
TomLga <semitallbox@gmail.com>
2023-05-26 15:02:44 +0200

    Notcomplete

--- script/code.js
+++ script/code.js
@@ -1,4 +1,6 @@
 let person = {
+
+
     fristName : "logan",
     lastName : "Thomas",
     age : 23,
@@ -18,4 +20,36 @@ let adress = {
 
 console.log(person)
 console.log(subjects);
-console.log(adress);
+console.log(adress)
+
+
+// question 3
+
+function calculate(math) {
+  let numb1 = parseFloat(document.getElementById("numb1").value);
+  let numb2 = parseFloat(document.getElementById("numb2").value);
+  let operation = document.getElementById("operation").value;
+  let resultLabel = document.getElementById("lblResult");
+
+      let result;
+      
+      switch(operation) {
+        case "+":
+          result = numb1 + numb2;
+          break;
+        case "-":
+          result = numb1 - numb2;
+          break;
+        case "*":
+          result = numb1 * numb2;
+          break;
+        case "/":
+          result = numb1 / numb2;
+          break;
+        default:
+          result = "try again";
+      }
+      
+      resultLabel.textContent = "Answer: " + result;
+      document.getElementById("resultBox");
+    }
